df8360fd18d6eafafd59afca2afc0e0d28c50693
DuongNT <duongnthn2312@gmail.com>
2024-02-19 23:31:19 +0700

    Add dead zone for zombie

--- Assets/Script/GameLossZone.cs
+++ Assets/Script/GameLossZone.cs
@@ -1,16 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Assets.Script.Zombies;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLossZone : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D other)
     {
+
         Debug.Log("Something entered this zone.");
-        if (other.gameObject.TryGetComponent<AbstractZombie>(out var zombie))
+        if (other.TryGetComponent<Zombie>(out Zombie zombie))
         {
             Debug.Log("You lose!");
+            Destroy(zombie.gameObject);
+            SceneManager.LoadSceneAsync(0);
         }
     }
+
 }
